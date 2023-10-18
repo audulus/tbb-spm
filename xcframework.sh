c44c0db928e41619a76bff1504a401dd473f3aa5
Taylor Holliday <taylor@audulus.com>
2023-10-17 18:37:40 -0700

    Create xcframework.sh

---
+++ xcframework.sh
@@ -0,0 +1,22 @@
+
+set -x
+rm -rf oneTBB-2021.10.0
+rm -f v2021.10.0.tar.gz 
+
+wget https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.10.0.tar.gz
+tar zxf v2021.10.0.tar.gz
+cd oneTBB-2021.10.0
+mkdir build && cd build
+cmake ..
+make -j 12
+
+cd ..
+
+mkdir build_ios && cd build_ios
+cmake .. \
+   -DCMAKE_SYSTEM_NAME=iOS \
+   -DCMAKE_Swift_COMPILER_FORCED=true \
+   -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 
+make -j 12
+
+cd ../../
