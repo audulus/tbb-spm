a37a81f9cb88458d62a84e71d4a19e318c5af46e
Taylor Holliday <taylor@audulus.com>
2023-11-10 17:14:01 -0800

    Update xcframework.sh

--- xcframework.sh
+++ xcframework.sh
@@ -6,24 +6,25 @@ rm -f v2021.10.0.tar.gz
 wget https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.10.0.tar.gz
 tar zxf v2021.10.0.tar.gz
 cd oneTBB-2021.10.0
+
+# macOS build
 mkdir build && cd build
 cmake ..
-make -j 12
+make -j 12 tbb
 
+# ios build
 cd ..
-
 mkdir build_ios && cd build_ios
 cmake .. \
    -DCMAKE_SYSTEM_NAME=iOS \
    -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 
-make -j 12
-
+make -j 12 tbb target=ios
 cd ..
 
 xcodebuild -create-xcframework \
-           -library build/appleclang_15.0_cxx11_64_relwithdebinfo/libtbb.dylib \
+           -library build/appleclang_15.0_cxx11_64_relwithdebinfo/libtbb.12.10.dylib \
            -headers include \
-           -library build_ios/appleclang_15.0_cxx11_64_relwithdebinfo/libtbb.dylib \
+           -library build_ios/appleclang_15.0_cxx11_64_relwithdebinfo/libtbb.12.10.dylib \
            -headers include \
            -output ../tbb.xcframework
 
