f8ae28972155e01100fce64f15a5f564d0c1c68d
Taylor Holliday <taylor@audulus.com>
2023-11-11 16:36:52 -0800

    Add TBB-Test app

---
+++ TBB-Test/TBB-Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TBB-Test
+//
+//  Created by Taylor Holliday on 11/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TBB-Test/TBB-Test/TBB_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TBB_TestApp.swift
+//  TBB-Test
+//
+//  Created by Taylor Holliday on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TBB_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
