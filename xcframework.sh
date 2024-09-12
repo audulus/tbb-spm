233cde9a65b70c5908aaa7939acf9420ee91069c
Taylor Holliday <wtholliday@gmail.com>
2024-09-12 11:52:30 -0700

    update oneTBB version

--- xcframework.sh
+++ xcframework.sh
@@ -1,14 +1,14 @@
 
 set -euxo pipefail
 
-rm -rf oneTBB-2021.10.0
-rm -rf v2021.10.0.tar.gz
+rm -rf oneTBB-2021.13.0
+rm -rf v2021.13.0.tar.gz
 rm -rf tbb.xcframework
 rm -rf tbb.xcframework.zip
 
 wget https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.13.0.tar.gz
-tar zxf v2021.10.0.tar.gz
-cd oneTBB-2021.10.0
+tar zxf v2021.13.0.tar.gz
+cd oneTBB-2021.13.0
 
 # Apply patch to header so the headers play nice with frameworks (ugh)
 patch include/oneapi/tbb/tbb_allocator.h ../tbb.patch
@@ -34,8 +34,8 @@ mkdir tbb.framework/Versions/A
 mkdir tbb.framework/Versions/A/Resources
 mkdir tbb.framework/Versions/A/Headers
 cp -R ../include/oneapi/tbb/ tbb.framework/Versions/A/Headers
-install_name_tool -id @executable_path/tbb.framework/tbb appleclang_15.0_cxx11_64_relwithdebinfo/libtbb.12.10.dylib
-lipo -create appleclang_15.0_cxx11_64_relwithdebinfo/libtbb.12.10.dylib -output tbb.framework/Versions/A/tbb
+install_name_tool -id @executable_path/tbb.framework/tbb appleclang_15.0_cxx11_64_relwithdebinfo/libtbb.12.13.dylib
+lipo -create appleclang_15.0_cxx11_64_relwithdebinfo/libtbb.12.13.dylib -output tbb.framework/Versions/A/tbb
 cp ../../Info-macOS.plist tbb.framework/Versions/A/Resources/Info.plist
 ln -s A tbb.framework/Versions/Current
 ln -s Versions/Current/tbb tbb.framework/tbb
@@ -55,8 +55,8 @@ mkdir tbb.framework
 mkdir tbb.framework/Headers
 cp -R ../include/oneapi/tbb/ tbb.framework/Headers
 cp ../../Info-iOS.plist tbb.framework/Info.plist
-install_name_tool -id @executable_path/Frameworks/tbb.framework/tbb appleclang_15.0_cxx11_64_relwithdebinfo/libtbb.12.10.dylib
-lipo -create appleclang_15.0_cxx11_64_relwithdebinfo/libtbb.12.10.dylib -output tbb.framework/tbb
+install_name_tool -id @executable_path/Frameworks/tbb.framework/tbb appleclang_15.0_cxx11_64_relwithdebinfo/libtbb.12.13.dylib
+lipo -create appleclang_15.0_cxx11_64_relwithdebinfo/libtbb.12.13.dylib -output tbb.framework/tbb
 
 cd ..
 
