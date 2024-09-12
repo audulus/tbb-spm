f2ee60cd96d82f6cb9b61b6d818716015dfd34b0
Taylor Holliday <wtholliday@gmail.com>
2024-09-12 14:51:07 -0700

    add basic tbb usage to test

--- TBB-Test/TBB-Test/tbb_test.cpp
+++ TBB-Test/TBB-Test/tbb_test.cpp
@@ -6,5 +6,20 @@
 //
 
 #include <stdio.h>
-#include "tbb/task.h"
 #include "tbb/tbb_allocator.h"
+#include "tbb/parallel_reduce.h"
+#include "tbb/blocked_range.h"
+
+void test() {
+    int sum = oneapi::tbb::parallel_reduce(oneapi::tbb::blocked_range<int>(1,101), 0,
+       [](oneapi::tbb::blocked_range<int> const& r, int init) -> int {
+          for (int v = r.begin(); v != r.end(); v++  ) {
+             init += v;
+          }
+          return init;
+       },
+       [](int lhs, int rhs) -> int {
+          return lhs + rhs;
+       }
+    );
+}
