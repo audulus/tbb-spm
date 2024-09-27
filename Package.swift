3b7877905112736ef6143e990ac9f07434d0abdf
Taylor Holliday <wtholliday@gmail.com>
2024-09-26 23:23:18 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "tbb",
-            url: "https://github.com/audulus/tbb-spm/releases/download/v13/tbb.xcframework.zip",
-            checksum: "e4dc6420df74d2c7af389a175a8714458a7a912eac72f4190eb46b7de9ff8471"
+            url: "https://github.com/audulus/tbb-spm/releases/download/v14/tbb.xcframework.zip",
+            checksum: "7d5a8677b51ac90f356d504ae03b082cabbe2a24cf76353688308263eb655510"
         ),
         .binaryTarget(
             name: "tbbmalloc",
-            url: "https://github.com/audulus/tbb-spm/releases/download/v13/tbbmalloc.xcframework.zip",
-            checksum: "241fb9b0fa72d61df07a11ad8af988c57b28ad7f489890c4fa3d98c59ad4ce49"
+            url: "https://github.com/audulus/tbb-spm/releases/download/v14/tbbmalloc.xcframework.zip",
+            checksum: "120e6c39d871f6485210160eedc374bccb3148704950caf3a65c6328bf2411f5"
         )
     ],
     cxxLanguageStandard: .cxx20
